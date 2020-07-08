be330c06e4ae936c15fcf8578d4e223b59bd4ce6
Jack Cherng <jfcherng@gmail.com>
2020-07-09 02:54:20 +0800

    chore: add some comments
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -7,7 +7,9 @@ from functools import lru_cache
 
 from . import tarball
 from LSP.plugin import AbstractPlugin
-from LSP.plugin.core.typing import Tuple, Optional
+from LSP.plugin.core.protocol import WorkspaceFolder
+from LSP.plugin.core.types import ClientConfig
+from LSP.plugin.core.typing import List, Tuple, Optional
 
 
 # @see https://github.com/latex-lsp/texlab/releases
@@ -27,11 +29,27 @@ def plugin_unloaded() -> None:
 
 @lru_cache()
 def get_plugin_cache_dir() -> str:
+    """
+    @brief Get this plugin's cache dir.
+
+    @return The cache dir.
+    """
+
     return os.path.join(sublime.cache_path(), PLUGIN_NAME)
 
 
 @lru_cache()
 def get_server_download_url(version: str, arch: str, platform: str) -> Optional[str]:
+    """
+    @brief Get the LSP server download URL.
+
+    @param version  The LSP server version
+    @param arch     The arch ("x32" or "x64")
+    @param platform The platform ("osx", "linux" or "windows")
+
+    @return The LSP server download URL.
+    """
+
     url_pattern = "https://github.com/latex-lsp/texlab/releases/download/{}/{}"
 
     if arch == "x64" and platform == "osx":
@@ -48,6 +66,12 @@ def get_server_download_url(version: str, arch: str, platform: str) -> Optional[
 
 @lru_cache()
 def get_server_dir() -> str:
+    """
+    @brief Get the LSP server dir.
+
+    @return The LSP server dir.
+    """
+
     arch = sublime.arch()
     platform = sublime.platform()
     server_dir = "{}-{}-{}".format(platform, arch, TEXLAB_VERSION)
@@ -57,6 +81,12 @@ def get_server_dir() -> str:
 
 @lru_cache()
 def get_server_bin_path() -> str:
+    """
+    @brief Get the LSP server binary path.
+
+    @return The LSP server binary path.
+    """
+
     platform = sublime.platform()
 
     if platform == "windows":
@@ -69,6 +99,12 @@ def get_server_bin_path() -> str:
 
 @lru_cache()
 def is_plugin_supported() -> bool:
+    """
+    @brief Determine if plugin can run on this machine.
+
+    @return True if plugin supported, False otherwise.
+    """
+
     arch = sublime.arch()
     platform = sublime.platform()
 
@@ -91,7 +127,13 @@ class LspLatexPlugin(AbstractPlugin):
         return settings, settings_path
 
     @classmethod
-    def can_start(cls, window, initiating_view, workspace_folders, configuration) -> Optional[str]:
+    def can_start(
+        cls,
+        window: sublime.Window,
+        initiating_view: sublime.View,
+        workspace_folders: List[WorkspaceFolder],
+        configuration: ClientConfig,
+    ) -> Optional[str]:
         if not is_plugin_supported():
             return "Unsupported platform"
         return None
@@ -118,5 +160,11 @@ class LspLatexPlugin(AbstractPlugin):
 
     @classmethod
     def cleanup_cache(cls) -> None:
+        """
+        @brief Clean up this plugin's cache directory.
+
+        @param cls The cls
+        """
+
         shutil.rmtree(get_plugin_cache_dir(), ignore_errors=True)
         os.makedirs(get_server_dir(), exist_ok=True)
