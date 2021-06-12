8e6dfe79a057a2b654f472df655cba5f042a60b5
Jack Cherng <jfcherng@gmail.com>
2021-06-12 22:51:08 +0800

    chore: update TexLab v3.2.0
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- const.py
+++ const.py
@@ -2,7 +2,7 @@ import sublime
 
 
 # @see https://github.com/latex-lsp/texlab/releases
-SERVER_VERSION = "v3.1.0"
+SERVER_VERSION = "v3.2.0"
 
 PLUGIN_NAME = "LSP-TexLab"
 SETTINGS_FILENAME = "{}.sublime-settings".format(PLUGIN_NAME)
