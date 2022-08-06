14a108e4ac4cc59f4088ae37fbf7c6f08699b6e6
Jack Cherng <jfcherng@gmail.com>
2022-08-06 08:23:26 +0800

    chore: update supported platform/arch list
    https://github.com/latex-lsp/texlab/releases/tag/v4.2.1
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- const.py
+++ const.py
@@ -8,7 +8,14 @@ PLUGIN_NAME = "LSP-TexLab"
 SETTINGS_FILENAME = "{}.sublime-settings".format(PLUGIN_NAME)
 
 # these platforms have an official pre-built texlab binary on GitHub
-MANAGED_PLATFORM_ARCHS = {"linux_x64", "osx_arm64", "osx_x64", "windows_x64"}
+MANAGED_PLATFORM_ARCHS = {
+    "linux_arm64",
+    "linux_x64",
+    "osx_arm64",
+    "osx_x64",
+    "windows_x32",
+    "windows_x64",
+}
 
 ARCH = sublime.arch()
 PLATFORM = sublime.platform()
--- server.py
+++ server.py
@@ -27,9 +27,11 @@ def get_server_download_url(version: str, platform: str, arch: str) -> Optional[
     """
 
     tarball_names = {
+        "linux_arm64": "texlab-aarch64-linux.tar.gz",
         "linux_x64": "texlab-x86_64-linux.tar.gz",
         "osx_arm64": "texlab-aarch64-macos.tar.gz",
         "osx_x64": "texlab-x86_64-macos.tar.gz",
+        "windows_x32": "texlab-i686-windows.zip",
         "windows_x64": "texlab-x86_64-windows.zip",
     }
 
